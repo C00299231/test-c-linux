cb3f0082b547de59ac373b7e013ca51045ef7eef
Adam N <C00299231@setu.ie>
2025-09-11 22:08:41 +0100

    init commit

---
+++ testc.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if(pid < 0) // error occurred
+    {
+        fprintf(stderr, "Fork failed.");
+        return 1;
+    }
+    else if(pid == 0) // child process
+    {
+        execlp("/bin/ls","ls", NULL);
+    }
+    else // parent will wait for child to complete
+    {
+        wait(NULL);
+        printf("Child Complete");
+    }
+
+    return 0;
+}
