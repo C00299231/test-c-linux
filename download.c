af1d54a0035788c08384bb841a4d48611a197349
Adam N <C00299231@setu.ie>
2025-10-05 16:03:14 +0100

    added

--- download.c
+++ download.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
+// STEPS TO PERFORM:
+// get link from file
+// download file from internet, save data to new file
+
 char links[30][100];
 
 const char linksFile[] = "filesToDownload.txt";
 
-int readLinks(char[]);
+int readLinks(const char[]);
+int writeToFile(const char*, char[30][100]);
 
 int main() // curl DL code fromstackoverflow.com/questions/11471690/curl_h_no_such_file_or_directory
 {
-    if(!readLinks(linksFile))
+    if(!readLinks(linksFile)) // get web links from file
     {
-        printf(stderr, "READING FAILED! Exiting...\n");
+        printf("READING FAILED! Exiting...\n");
         return 1;
     }
-
-    CURL *curl;
-    FILE *fp;
-    CURLcode res;
-
     const char *outFileName = "/home/user/Desktop/testOutput/";
 
+    CURL *curl;
+    FILE *fp = fopen("newFile", "wb");
+
+    
+    // initialize curl
     curl = curl_easy_init();
     if(!curl)
     {
-        printf(stderr, "CURL INIT FAILED!");
+        printf("CURL INIT FAILED!");
         return 1;
     }
-
+    
+    // set curl options, perform curl
     curl_easy_setopt(curl, CURLOPT_URL, links[0]);
-
-    res = curl_easy_perform(curl);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    CURLcode res = curl_easy_perform(curl);
 
     if(res != CURLE_OK)
     {
@@ -42,13 +48,13 @@ int main() // curl DL code fromstackoverflow.com/questions/11471690/curl_h_no_su
     return 0;
 }
 
-int readLinks(char path[]) // file reading code from www.w3schools.com/c/c_files_read.php
+int readLinks(const char path[]) // file reading code from www.w3schools.com/c/c_files_read.php
 {
     FILE *filePtr = fopen(path, "r");
 
     if(filePtr == NULL)
     {
-        // no file!
+        // no file! failure
         printf("\nUnable to open the specified file.\n");
         return 0;
     }
@@ -57,32 +63,30 @@ int readLinks(char path[]) // file reading code from www.w3schools.com/c/c_files
 
     while(fgets(links[currentLine], 100, filePtr) && currentLine < 29)
     {
-        // stored string!
+        // stored string! nothing else to do here :/
     }
-
-
+    
     fclose(filePtr);
 
     return 1;
 }
 
-int writeToFile(const char *filename, const char *data)
+int writeToFile(const char *filename, char data[30][100])
 {
-    FILE *filePtr = fopen(filename, "w");//
+    FILE *filePtr = fopen(filename, "w");
     if(filePtr == NULL)
     {
         // no file!
         printf("\nUnable to open the specified file.\n");
         return 0;
     }
-    fputs(content, filePtr);
-    fclose(filePtr);
-    return 1; // success
-}
 
-const char getFullPath(const char *filename)
-{
-    char fullPath[200];
-    snprintf(fullPath, sizeof(fullPath), "%s/%s", linksFile, filename);
-    return fullPath;
+    for(int idx = 0; idx < 30; idx++)
+    {
+        fprintf(filePtr, "%s\n", data[idx]);
+    }
+
+    fclose(filePtr);
+
+    return 1; // success
 }
